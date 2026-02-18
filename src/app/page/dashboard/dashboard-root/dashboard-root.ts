26236d403d0c6dce4a98f6d24f48d7119a1536eb
Pramudithalakshan <pramudithalakshan00@gmail.com>
2026-02-18 14:10:59 +0530

    Implement router nativagation - Login - Dashboard

---
+++ src/app/page/dashboard/dashboard-root/dashboard-root.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard-root',
+  imports: [RouterLink],
+  templateUrl: './dashboard-root.html',
+  styleUrl: './dashboard-root.css',
+})
+export class DashboardRoot {
+
+}
