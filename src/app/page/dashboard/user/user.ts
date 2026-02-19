84248f6873bb8e0852341231dac59fee769475f1
Pramudithalakshan <pramudithalakshan00@gmail.com>
2026-02-19 11:53:48 +0530

    Implement user data loading - Add service layer - Add ChangeDetectorRef to detect changes in UI

--- src/app/page/dashboard/user/user.ts
+++ src/app/page/dashboard/user/user.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { UserService } from './user.service';
 
 @Component({
@@ -10,8 +10,10 @@ import { UserService } from './user.service';
   styleUrl: './user.css',
 })
 export class User implements OnInit{
+private cdr = inject(ChangeDetectorRef)
 private userService = inject(UserService);
 public users: any[] = [];
+
 ngOnInit(): void {
   this.loadUsers();
 }
@@ -20,6 +22,7 @@ loadUsers(){
     next:(data)=>{
       this.users =data;
       console.log('Data recived', this.users)
+      this.cdr.detectChanges();
     }
   })
 }
